f356fc99af0de320d0af6077895dc8419606ba95
Nimuthu Methsen GANEGODA <nimuthuganegoda@gmail.com>
2025-11-20 07:21:14 +0000

    Update next.config for production deployment with basePath

--- next.config.js
+++ next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/glowing-happiness' : '',
+  basePath: '/glowing-happiness',
+  assetPrefix: '/glowing-happiness',
 }
 
 module.exports = nextConfig
